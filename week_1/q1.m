97ac6a648617ea542ee259f2571164a0f8b5de13
Genevieve Clifford <genevieve@becquerel.me>
2020-01-15 15:46:34 +0000

    added fprintf to print data

--- week_1/q1.m
+++ week_1/q1.m
@@ -10,6 +10,9 @@ b=[120;90];
 lb=[0;0];
 ub=[70;50];
 
-[x,fval]=linprog(-P,A,b,[],[],lb,ub)
+[x,fval]=linprog(-P,A,b,[],[],lb,ub);
 
 fval=-fval;
+
+formatSpec='A profit of £%4.2f is to be made by producing %2.0f low-cost and %2.0f high-cost units respectively';
+fprintf(formatSpec,fval,x(1),x(2))
