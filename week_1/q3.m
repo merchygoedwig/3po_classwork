c1f5ddea5d83b3e3296ba0bf4c5b05a6c7d5cbfe
Genevieve Clifford <genevieve@becquerel.me>
2020-01-16 22:23:28 +0000

    completed q3

--- week_1/q3.m
+++ week_1/q3.m
@@ -3,3 +3,48 @@
 %sxc1065@student.bham.ac.uk / genevieve@becquerel.me
 
 clear all; close all; clc;
+
+m=5;
+
+cost=[10;15;2];
+nC=length(cost);
+unitLim=[2000;600];
+nuL=length(unitLim);
+compConstrVar=[-900;-2100;-2400;-3000;-4300];
+lb=zeros(nC*m,1);
+lb(end)=300;
+
+f=zeros(m,1);
+
+for i=1:nC
+    a=cost(i)*ones(m,1);
+    f(1+(i-1)*m:m+(i-1)*m,1)=a;
+end
+
+simpConstr=[eye(nuL*m) zeros(nuL*m,m)];
+
+simpConstrVar=zeros(m*nuL,1);
+
+for j=1:nuL
+    simpConstrVar(1+(j-1)*m:m+(j-1)*m,1)=ones(m,1)*unitLim(j);
+end
+
+construct1=zeros(m);
+for k=1:m-1
+    construct1(k+1,k)=1;
+end
+
+compConstr=[-eye(m) -eye(m) eye(m)-construct1];
+compConstr(end)=0;
+
+constr=[simpConstr;compConstr];
+constrVar=vertcat(simpConstrVar,compConstrVar);
+
+[x,fval]=linprog(f,constr,constrVar,[],[],lb);
+x=x';
+
+formatSpec='There is a total cost of £%6.2f with units produced internally, externally and stored below.\n';
+fprintf(formatSpec,fval)
+
+T=array2table(x);
+T.Properties.VariableNames={'IUM1','IUM2','IUM3','IUM4','IUM5','EUM1','EUM2','EUM3','EUM4','EUM5','NS1','NS2','NS3','NS4','NS5'}
